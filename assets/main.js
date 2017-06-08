1bb0e25b1bbbdfcd7896a41739d0b5a17b0a7016
hasmikcoder <hasvart@gmail.com>
2017-06-07 20:09:12 -0500

    Reusable header and footer

---
+++ assets/main.js
@@ -0,0 +1,50 @@
+var header = '<div class="nav-container">'+
+  '<div class="nav-link">'               +
+        '<a href="menu.html"> Menu </a>'           +
+   '</div>'                               +
+  '<div class="nav-link">'                          +
+   '<a href="custom.html">Custom</a>'             +
+  '</div>'                                           +
+
+  '<div class="fix-logo">'                          +
+        '<img src="assets/images/fix-logo.png">'    +
+  '</div>'                                           +
+
+  '<div class="nav-link">'                           +
+        '<a href="about.html">About</a>'             +
+  '</div>'                                           +
+
+  '<div class="nav-link">'                           +
+          '<a href="hours.html"> Hours</a>'          +
+  '</div>'                                           +
+'</div>' ;
+
+
+$("header").html(header);
+
+
+var footer =  '<div class= "container">'  +
+      '<div class="address">'             +
+        '<p>27 MARYLAND PLAZA<br>'        +
+         'SAINT LOUIS, MO 63108<br>'       +
+        '</p>'                              +
+
+         'Tues-Sat 7am-7pm, Sun 11am-4pm'    +
+
+     '</div>'                             +
+  '<div class="fix-logo-white">'           +
+
+  '<img src="assets/images/fix-logo-white.png"/>'  +
+'</div>'                                            +
+'<div class="fix-icon">'                             +
+  '<img src="assets/images/fix-fbicon.png"/>'        +
+'</div>'                                            +
+'<div class="fix-icon">'                            +
+  '<img src="assets/images/fix-twittericon.png"/>'   +
+'</div>'                                              +
+'<div class="fix-icon">'                             +
+'<img src="assets/images/fix-instaicon.png"/>'        +
+ '</div>'                                             +
+ '</div>';
+
+ $("footer").html(footer);
